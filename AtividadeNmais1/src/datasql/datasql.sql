927202c31609de2213166388e4b295ca9525ba39
Débora Peixoto <92814333+DeboraPeixoto@users.noreply.github.com>
2022-11-29 01:44:41 -0300

    Add files via upload

---
+++ AtividadeNmais1/src/datasql/datasql.sql
@@ -0,0 +1,10 @@
+INSERT INTO Proprietario(id, nome) VALUES (1, 'Maria');
+INSERT INTO Proprietario(id, nome) VALUES (2, 'Joao');
+INSERT INTO Proprietario(id, nome) VALUES (3, 'Fulano');
+
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (1, 'Carro1',1);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (2, 'Carro2',1);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (3, 'carro3',2);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (4, 'Carro4',3);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (5, 'Carro5',3);
+
